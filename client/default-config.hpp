d812515c08a28024e95b5055e7e88b2375555286
Sebastian <sebastian@sgundersen.com>
2019-04-27 19:31:36 +0200

    added config files

---
+++ client/default-config.hpp
@@ -0,0 +1,8 @@
+#pragma once
+
+namespace config {
+
+const std::string host = "game.einheri.xyz";
+const int port = 7524;
+
+}
---
+++ server/default-config.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+namespace config {
+
+const std::string host = "localhost";
+const int port = 7524;
+const int max_clients = 100;
+const int save_interval_seconds = 120;
+
+namespace database {
+
+const std::string name = "einheri";
+const std::string user = "einheri";
+const std::string password = "einheri";
+const std::string host = "localhost";
+const int port = 5432;
+
+}
+
+}
